192c634b18785bafe60a4d4dd433acb4018b7c52
umitkalemkus <umitkalemkus@yahoo.com>
2023-02-21 21:21:37 +0000

    done

--- src/test/java/pages/CustomerLoginPage.java
+++ src/test/java/pages/CustomerLoginPage.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utilities.BaseTestReport;
-import utilities.Driver;
+import utilites.BaseTestReport;
+import utilites.Driver;
+import utilites.BaseTestReport;
+import utilites.Driver;
 
 import java.time.Duration;
 
