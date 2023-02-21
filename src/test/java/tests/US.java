1ff67976407cfda9b67dd19768d653768a6e7309
mbaranv <gfbarann@gmail.com>
2023-02-22 01:31:17 +0300

    update

--- src/test/java/tests/US.java
+++ src/test/java/tests/US.java
@@ -4,5 +4,6 @@ public class US {
     public static void main(String[] args) {
         System.out.println("selam");
         System.out.println("selam");
+        System.out.println("selam");
     }
 }
